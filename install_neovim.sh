d73441aa89a90cfbf902d0d5ca9d6d814f656349
Steven Arcangeli <stevearc@stripe.com>
2023-08-28 08:04:12 -0700

    fix: more robust installation of Neovim stable version

--- install_neovim.sh
+++ install_neovim.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 set -e
 
-NVIM_TAG="$1"
-if [ -z "$NVIM_TAG" ]; then
-  NVIM_TAG=$(curl -s https://api.github.com/repos/neovim/neovim/releases/latest | jq -r .tag_name)
-fi
+NVIM_TAG="${1-stable}"
 echo "Installing Neovim $NVIM_TAG"
 curl -sL "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim.appimage" -o nvim.appimage
 chmod +x nvim.appimage
