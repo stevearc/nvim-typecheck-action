14754f305f315837a7f1c81a9d67e9b08a5872b6
Steven Arcangeli <stevearc@stripe.com>
2025-02-11 17:32:17 -0800

    fix: use new appimage name for nvim v0.10.4

--- install_neovim.sh
+++ install_neovim.sh
@@ -3,7 +3,12 @@ set -e
 
 NVIM_TAG="${1-stable}"
 echo "Installing Neovim $NVIM_TAG"
-curl -sL "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/nvim.appimage" -o nvim.appimage
+dl_name="nvim-linux-x86_64.appimage"
+# The appimage name changed in v0.10.4
+if python -c 'from packaging.version import Version; import sys; sys.exit(not (Version(sys.argv[1]) < Version("v0.10.4")))' "$NVIM_TAG" 2>/dev/null; then
+  dl_name="nvim.appimage"
+fi
+curl -sL "https://github.com/neovim/neovim/releases/download/${NVIM_TAG}/${dl_name}" -o nvim.appimage
 chmod +x nvim.appimage
 ./nvim.appimage --appimage-extract >/dev/null
 rm -f nvim.appimage
