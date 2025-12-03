a2c7bff1c2f1d8489106271c8f89183628d41655
Steven Arcangeli <stevearc@stevearc.com>
2025-12-02 23:07:40 -0800

    fix: support installing luaLS on arm64 linux

--- install_luals.sh
+++ install_luals.sh
@@ -7,7 +7,8 @@ cd ~/.local/share/language-servers/lua-language-server
 if [ -z "$VERSION" ]; then
   VERSION=$(curl -s https://api.github.com/repos/LuaLS/lua-language-server/releases/latest | jq -r .tag_name)
 fi
-curl -sL "https://github.com/LuaLS/lua-language-server/releases/download/${VERSION}/lua-language-server-${VERSION}-linux-x64.tar.gz" -o lua-language-server.tar.gz
+arch="$([ "$(uname -m)" = 'x86_64' ] && echo 'x64' || echo 'arm64')"
+curl -sL "https://github.com/LuaLS/lua-language-server/releases/download/${VERSION}/lua-language-server-${VERSION}-linux-${arch}.tar.gz" -o lua-language-server.tar.gz
 tar -zxf lua-language-server.tar.gz
 rm -f lua-language-server.tar.gz
 ./bin/lua-language-server --version
